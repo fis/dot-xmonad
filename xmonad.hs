10e2e708dcd52a7a4006a3de1f7c26d4860a32e5
Heikki Kallasjoki <fis@zem.fi>
2011-08-12 15:57:26 +0300

    Some dzen2 hackery

--- xmonad.hs
+++ xmonad.hs
@@ -6,11 +6,16 @@ import XMonad.Layout.IndependentScreens
 import qualified XMonad.StackSet as W
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+
+import Control.Applicative
 import System.IO
+import System.Process
 
 myModm = mod4Mask
-myTerminal = "urxvt"
+myTerminal = "urxvtc"
+myDzen = "/home/htkallas/local/bin/dzen2"
 
+myScreens = [(1280,1024), (1600,1200)]
 myWorkspaces = ["web", "com", "work1", "work2", "x1", "x2", "x3", "x4"]
 
 myKeys =
@@ -27,17 +32,30 @@ myKeys =
     ]
 
 main = do
-    dzenproc <- spawnPipe "/home/htkallas/local/bin/dzen2 -ta l"
+    (toDzen0, fromDzen0) <- myRunDzen 0
+    (toDzen1, fromDzen1) <- myRunDzen 1
     xmonad $ gnomeConfig {
-        workspaces = withScreens 2 myWorkspaces,
+        workspaces = withScreens (S $ length myScreens) myWorkspaces,
         modMask = myModm,
         terminal = myTerminal,
         logHook = do
             dynamicLogWithPP dzenPP {
-                ppOutput = hPutStrLn dzenproc
+                ppOutput = \s -> sequence_ $ [hPutStrLn toDzen0, hPutStrLn toDzen1] <*> [s]
                 }
             logHook gnomeConfig
-        -- manageHook = manageDocks <+> manageHook gnomeConfig,
-        -- layoutHook = avoidStruts $ layoutHook gnomeConfig
         }
         `additionalKeys` myKeys
+
+-- dzen spawning for different screens
+
+myRunDzen :: Int -> IO (Handle, Handle)
+myRunDzen s = do
+    (inp, out, err, pid) <- runInteractiveProcess myDzen args Nothing Nothing
+    return (inp, out)
+  where
+    args :: [String]
+    args = ["-ta", "l", "-p", "-x", xpos, "-y", "0", "-h", "16", "-w", width]
+    xpos :: String
+    xpos = show . sum $ fst `map` take s myScreens
+    width :: String
+    width = show . fst $ myScreens !! s
