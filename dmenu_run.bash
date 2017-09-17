2f92ce7142163b3b3df6dc107672df25056cb02d
Heikki Kallasjoki <fis@zem.fi>
2017-09-17 13:15:59 +0100

    Define font size for mod-r dmenu.

--- dmenu_run.bash
+++ dmenu_run.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cmd="$(dmenu_path | dmenu -l 16)"
+cmd="$(dmenu_path | dmenu -fn 'DejaVu Sans Mono:size=9' -l 16)"
 if [[ $? != 0 ]]; then exit 1; fi
 if [[ "$cmd" =~ ^[a-z0-9/_-]*$ ]]; then exec "$cmd"; exit 1; fi
 "$cmd"
